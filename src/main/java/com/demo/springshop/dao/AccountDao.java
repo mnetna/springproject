4ae966fbb1a5e47c4dceb1980cbf1625772afdfd
HF195 <sssolee90@gmail.com>
2019-05-13 14:34:20 +0900

    2019.05.13 PostgreSQL Connection 추가

--- src/main/java/com/demo/springshop/dao/AccountDao.java
+++ src/main/java/com/demo/springshop/dao/AccountDao.java
@@ -2,9 +2,11 @@ package com.demo.springshop.dao;
 
 import com.demo.springshop.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountDao extends JpaRepository<Account, Long> {
     Optional<Account> findByUsername(String username);
 }
