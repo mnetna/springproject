244b0117e106bf16ecedb48293739b338307ed2c
이솔 <mnenta>
2020-02-27 17:45:08 +0900

    2020.02.27 google oauth2 설정

--- src/main/java/com/sales/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/sales/springboot/config/auth/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.sales.springboot.config.auth;
 
 import com.sales.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity //Spring Security 기능 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -14,10 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().headers().frameOptions().disable()
-                .and().authorizeRequests()                                                                      // URL 권한 관리 시작
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 권한 관리 대상 지정, 모든 URL에 대해 열람 권한 지정
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name()).anyRequest().authenticated()  // "/api/vi/**" URL에 대해서 USER 권한이 있는 대상만 접근 가능
-                .and().logout().logoutSuccessUrl("/")                                                           // 로그아웃 진입점
-                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);                   //OAuth2 로그인 성공 이후 후속 조치 수행
+                .and().authorizeRequests()
+                .antMatchers("/", "/register", "/css/**", "/images/**", "/js/**", "/scss/**", "/vendor/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()).anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/")
+                .and().oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
     }
 }
